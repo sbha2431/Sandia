d75844a4cd412009ffcd4791eb767e733deeb4e2
rayna <rayna@mpi-sws.org>
2017-05-17 19:05:18 +0200

    example with simple refinement

--- Code/1agent_1target_6x6_refinement.py
+++ Code/1agent_1target_6x6_refinement.py
@@ -31,36 +31,49 @@ gwg.render()
 gwg.draw_state_labels()
 gwg.save('gridworldfig_6x6.png')
 
-beliefparts = 2
-beliefcons = 2
-partition = grid_partition.partitionGrid(gwg,beliefparts);#
-print (partition)
-
 outfile = 'output_6x6.json'
 infile = 'input_6x6'
-print ('Writing slugs input file...')
-Salty_input.write_to_slugs_part(infile,gwg,moveobstacles[0],1, partition,beliefcons)
-print ('Converting input file...')
-os.system('python compiler.py ' + infile + '.structuredslugs > ' + infile + '.slugsin')
-print('Computing controller...')
-result = subprocess.Popen('/home/rayna/work/tools/slugs/src/slugs --counterStrategy ' + infile+'.slugsin > counterexample_6x6.txt',shell=True, stdout=subprocess.PIPE)
+cexfile = 'counterexample_6x6.txt'
+slugs = '/home/rayna/work/tools/slugs/src/slugs'
 
-time.sleep(10)
-
-(refineLeaf,leafBelief) = counterexample_parser.analyse_counterexample('counterexample_6x6.txt',gwg,partition,beliefcons)
-# select successors 2, 4
-
-print (refineLeaf)
-print (leafBelief)
-
-for k in refineLeaf:
-    partition = grid_partition.partitionState_manual(partition,k,leafBelief)
-
+beliefparts = 2
+beliefcons = 2
+partition = grid_partition.partitionGrid(gwg,beliefparts);
 print (partition)
 
+done= False
+
+while (not done):
+
+	initial = [28]
+	moveobstacles = [10]
+	gwg = Gridworld(initial, nrows, ncols,nagents, targets, obstacles, moveobstacles,regions)
+	gwg.colorstates = [set(),set()]
+
+	print ('Writing slugs input file...')
+	Salty_input.write_to_slugs_part(infile,gwg,moveobstacles[0],1, partition,beliefcons)
+	print ('Converting input file...')
+	os.system('python compiler.py ' + infile + '.structuredslugs > ' + infile + '.slugsin')
+	print('Checking realizability...')
+	subprocess.Popen(slugs + ' --counterStrategy ' + infile+'.slugsin > ' + cexfile,shell=True, stdout=subprocess.PIPE)
+
+	time.sleep(10)
+
+	done = (os.stat(cexfile).st_size == 0)
+	
+	if (done):
+		break;
+	
+	(refineLeaf,leafBelief) = counterexample_parser.analyse_counterexample(cexfile,gwg,partition,beliefcons)
+	print (refineLeaf)
+	print (leafBelief)
+
+	for k in refineLeaf:
+		partition = grid_partition.partitionState_manual(partition,k,leafBelief)
+
+	print (partition)
+
 initial = [28]
-targets = [[ncols+1]]
-obstacles = [14,15,20,21]
 moveobstacles = [10]
 gwg = Gridworld(initial, nrows, ncols,nagents, targets, obstacles, moveobstacles,regions)
 gwg.colorstates = [set(),set()]
@@ -69,30 +82,5 @@ Salty_input.write_to_slugs_part(infile,gwg,moveobstacles[0],1, partition,beliefc
 print ('Converting input file...')
 os.system('python compiler.py ' + infile + '.structuredslugs > ' + infile + '.slugsin')
 print('Computing controller...')
-result = subprocess.Popen('/home/rayna/work/tools/slugs/src/slugs --counterStrategy ' + infile+'.slugsin > counterexample_6x6.txt',shell=True, stdout=subprocess.PIPE)
+subprocess.Popen(slugs + ' --explicitStrategy --jsonOutput ' + infile + '.slugsin > '+ outfile,shell=True, stdout=subprocess.PIPE)
 
-time.sleep(10)
-
-(refineLeaf,leafBelief) = counterexample_parser.analyse_counterexample('counterexample_6x6.txt',gwg,partition,beliefcons)
-# select successors 2, 9
-
-print (refineLeaf)
-print (leafBelief)
-
-for k in refineLeaf:
-    partition = grid_partition.partitionState_manual(partition,k,leafBelief)
-
-print (partition)
-
-initial = [28]
-targets = [[ncols+1]]
-obstacles = [14,15,20,21]
-moveobstacles = [10]
-gwg = Gridworld(initial, nrows, ncols,nagents, targets, obstacles, moveobstacles,regions)
-gwg.colorstates = [set(),set()]
-
-Salty_input.write_to_slugs_part(infile,gwg,moveobstacles[0],1, partition,beliefcons)
-print ('Converting input file...')
-os.system('python compiler.py ' + infile + '.structuredslugs > ' + infile + '.slugsin')
-print('Computing controller...')
-result = subprocess.Popen('/home/rayna/work/tools/slugs/src/slugs --explicitStrategy --jsonOutput ' + infile + '.slugsin > '+ outfile,shell=True, stdout=subprocess.PIPE)
